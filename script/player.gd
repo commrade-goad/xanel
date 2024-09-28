2cb0a55078d6f77d921b2c1832b4b7b0a153b4a1
Fernando Perry <fernandoperry1234@gmail.com>
2024-09-28 16:30:53 +0700

    adde sword follow mouse

--- script/player.gd
+++ script/player.gd
@@ -2,20 +2,19 @@ extends Node2D
 
 signal hit
 @export var roll_speed = 2000
-@export var speed = 300
-@export var friction = 0.2 # Friction value to slow down movement smoothly
+@export var speed = 350
+@export var friction = 0.2 
 var screen_size
 var velocity = Vector2.ZERO
 var rolling = false
+var attacking = false
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 
-	# Handle basic movement input
 	var input_velocity = Vector2.ZERO
 	if Input.is_action_pressed("p_right"):
 		input_velocity.x += 1
@@ -26,11 +25,9 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("p_down"):
 		input_velocity.y += 1
 
-	# Normalize the input to ensure uniform speed in diagonal directions
 	if input_velocity.length() > 0:
 		input_velocity = input_velocity.normalized() * speed
-	
-	# Add roll speed on rolling action (triggered only once per press)
+
 	if Input.is_action_just_pressed("p_roll") and rolling == false:
 		if Input.is_action_pressed("p_right"):
 			input_velocity.x += roll_speed
@@ -43,14 +40,11 @@ func _process(delta: float) -> void:
 		rolling = true
 		$roll_timer.start()
 
-	# Apply movement velocity with gradual deceleration
 	velocity = velocity.lerp(input_velocity, friction)
 
-	# Move the player and limit to the screen boundaries
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
 
-	# Flip sprite based on movement direction
 	if velocity.x != 0:
 		$player_sprite.flip_h = velocity.x < 0
 	
@@ -58,6 +52,23 @@ func _process(delta: float) -> void:
 		$player_sprite.animation = "roll"
 	else :
 		$player_sprite.animation = "idle"
+
+	var mouse_pos = get_global_mouse_position()
+
+	# Get the player's global position (after camera movement)
+	var player_global_pos = $player_sprite.global_position
+	# Calculate angle between player and mouse
+	var angle = atan2(mouse_pos.y - player_global_pos.y, mouse_pos.x - player_global_pos.x)
+
+	# Set the sword's position relative to the player, using global positions
+	var offset = Vector2(cos(angle), sin(angle)) * 30  # Adjust the distance as needed
+	$p_sword.global_position = player_global_pos + offset
+
+	$p_sword/sword_sprite.flip_v = angle >= 1.5 or angle <= -1.5
+	# Rotate the sword to face the mouse
+	$p_sword.rotation = angle + 45
+	if angle >= 1.5 or angle <= -1.5 :
+		$p_sword.rotation = angle -45
 
 
 func _on_roll_timer_timeout() -> void:
