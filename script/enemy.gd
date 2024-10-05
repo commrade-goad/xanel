aa6b8acf5dd3ed5728d914abd58448a76cc09a20
Fernando Perry <fernandoperry1234@gmail.com>
2024-10-05 23:06:59 +0700

    enemy behaviour update

--- script/enemy.gd
+++ script/enemy.gd
@@ -9,6 +9,8 @@ var player: CharacterBody2D
 var active: String
 var attack_sprite: AnimatedSprite2D
 var attack_coll: CollisionShape2D
+var plen
+var can_attack = false
 
 func _ready() -> void:
     attack_sprite = $enemy_a/slash_sprite
@@ -22,9 +24,13 @@ func _process(delta: float) -> void:
     attack_sprite.z_index = 101
     if player:
         var diff = player.position - position
-        var plen = diff.length()
+        plen = diff.length()
         var sprite = get_node(active + "/sprite")
         sprite.flip_h = diff.x < 0
+
+        if plen <= 50 and can_attack == true:
+            $enemy_a/slash_sprite.play()
+            $enemy_a/slash_cooldown.start()
 
         var angle = atan2(player.global_position.y - global_position.y, player.global_position.x - global_position.x)
         var offset = Vector2(cos(angle), sin(angle)) * 30
@@ -46,15 +52,25 @@ func _process(delta: float) -> void:
             attack_coll.disabled = false
 
         if attack_sprite.frame == 10:
+            can_attack = false
             $enemy_a/slash_cooldown.start()
             $enemy_a/slash_sprite.stop()
 
 
-        if plen > 50:
+        var distance = 50
+        if can_attack == false:
+            distance += 100
+        if plen > distance :
             var direction = diff.normalized()
             var input_velocity = direction * speed
             velocity = velocity.lerp(input_velocity, friction)
             position += velocity * delta
+        elif can_attack == false and plen < distance - 50:
+            var direction = -diff.normalized()
+            var input_velocity = direction * speed
+            velocity = velocity.lerp(input_velocity, friction)
+            position += velocity * delta
+
 
 func _on_slash_cooldown_timeout():
-    $enemy_a/slash_sprite.play()
+    can_attack = true
--- script/main.gd
+++ script/main.gd
@@ -5,10 +5,15 @@ func _ready() -> void:
     $player.position = Vector2(0,0)
     $bg.position = Vector2(-16, -16 * 2)
     $bg/higher_tile.position = Vector2(0, 16 *2)
+    $bg/higher_tile.z_index = 102
+    $bg/outer_black_left.z_index = 102
+    $bg/outer_black_right.z_index = 102
+    $bg/outer_black_bottom.z_index = 102
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
     $player.position = $player.position.clamp(Vector2(0, 0), Vector2(150 * 16, 102 * 16))
+    $enemy.position = $enemy.position.clamp(Vector2(0, 0), Vector2(150 * 16, 102 * 16))
     $camera.position = $player.position
 
     $player.z_index = clamp(int($player.position.y / 10), 1, 100)
