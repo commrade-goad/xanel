255fac5dd8eb2fbef444ff269e706180b3b12b62
Fernando Perry <fernandoperry1234@gmail.com>
2024-10-06 15:46:08 +0700

    fixed z-index and other stuff

--- script/enemy.gd
+++ script/enemy.gd
@@ -1,40 +1,65 @@
 extends Node2D
 
 signal hit
-@export var speed = 250
+@export var speed = 270
 @export var friction = 0.2
 @export var hp = 100
 var velocity = Vector2.ZERO
 var player: CharacterBody2D
 var active: String
+var plen
+var offset
 var attack_sprite: AnimatedSprite2D
 var attack_coll: CollisionShape2D
-var plen
+var attack_cooldown: Timer
+var arm_sprite = Sprite2D
 var can_attack = false
+var lock_angle = false
 
 func _ready() -> void:
-    attack_sprite = $enemy_a/slash_sprite
-    attack_coll = $enemy_a/slash_coll
+    active = "enemy_a" 
+    attack_sprite = get_node(active + "/slash_sprite")
+    attack_coll = get_node(active + "/slash_coll")
+    attack_cooldown = get_node(active + "/slash_cooldown")
+    arm_sprite = get_node(active + "/arm")
     attack_sprite.play()
     player = get_parent().get_node("player")
-    active = "enemy_a" 
 
 func _process(delta: float) -> void:
     attack_sprite.global_position = position
     attack_sprite.z_index = 101
     if player:
+        # movement
         var diff = player.position - position
         plen = diff.length()
         var sprite = get_node(active + "/sprite")
         sprite.flip_h = diff.x < 0
 
         if plen <= 50 and can_attack == true:
-            $enemy_a/slash_sprite.play()
-            $enemy_a/slash_cooldown.start()
+            attack_sprite.play()
+            attack_cooldown.start()
 
-        var angle = atan2(player.global_position.y - global_position.y, player.global_position.x - global_position.x)
-        var offset = Vector2(cos(angle), sin(angle)) * 30
-        attack_sprite.rotation = angle
+        var distance = 50
+        if can_attack == false:
+            distance += 100
+        if plen > distance :
+            var direction = diff.normalized()
+            var input_velocity = direction * speed
+            velocity = velocity.lerp(input_velocity, 0.2)
+            if lock_angle == false:
+                position += velocity * delta
+        elif can_attack == false and plen < distance - 50:
+            var direction = -diff.normalized()
+            var input_velocity = direction * speed
+            velocity = velocity.lerp(input_velocity, 0.2)
+            if lock_angle == false:
+                position += velocity * delta
+
+        # attack
+        if lock_angle == false:
+            var angle = atan2(player.global_position.y - global_position.y, player.global_position.x - global_position.x)
+            offset = Vector2(cos(angle), sin(angle)) * 30
+            attack_sprite.rotation = angle
 
         if diff.x < 0 :
             attack_sprite.global_position += offset
@@ -45,6 +70,12 @@ func _process(delta: float) -> void:
 
         attack_coll.global_position = attack_sprite.global_position
         attack_coll.rotation = attack_sprite.rotation
+        arm_sprite.global_position = attack_sprite.global_position - offset
+
+        arm_sprite.rotation = attack_sprite.rotation
+
+        if attack_sprite.frame == 1:
+            lock_angle = true
 
         if attack_sprite.frame <= 3 or attack_sprite.frame >= 8:
             attack_coll.disabled = true
@@ -53,23 +84,9 @@ func _process(delta: float) -> void:
 
         if attack_sprite.frame == 10:
             can_attack = false
-            $enemy_a/slash_cooldown.start()
-            $enemy_a/slash_sprite.stop()
-
-
-        var distance = 50
-        if can_attack == false:
-            distance += 100
-        if plen > distance :
-            var direction = diff.normalized()
-            var input_velocity = direction * speed
-            velocity = velocity.lerp(input_velocity, friction)
-            position += velocity * delta
-        elif can_attack == false and plen < distance - 50:
-            var direction = -diff.normalized()
-            var input_velocity = direction * speed
-            velocity = velocity.lerp(input_velocity, friction)
-            position += velocity * delta
+            lock_angle = false
+            attack_cooldown.start()
+            attack_sprite.stop()
 
 
 func _on_slash_cooldown_timeout():
--- script/main.gd
+++ script/main.gd
@@ -5,10 +5,6 @@ func _ready() -> void:
     $player.position = Vector2(0,0)
     $bg.position = Vector2(-16, -16 * 2)
     $bg/higher_tile.position = Vector2(0, 16 *2)
-    $bg/higher_tile.z_index = 102
-    $bg/outer_black_left.z_index = 102
-    $bg/outer_black_right.z_index = 102
-    $bg/outer_black_bottom.z_index = 102
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
