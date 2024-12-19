33d7831d1de98cd3049571b4aba741d7400113b0
Fernando Perry <fernandoperry1234@gmail.com>
2024-12-19 12:09:44 +0700

    yes

--- script/levelup.gd
+++ script/levelup.gd
@@ -11,40 +11,40 @@ var obj_backup
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	parents = get_parent()
-	parents.connect("current_upgrade", Callable(self, "_on_current_upgrade"))
+    parents = get_parent()
+    parents.connect("current_upgrade", Callable(self, "_on_current_upgrade"))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if selected == true:
-		$ColorRect/grid2/Button2.disabled = true
-		$ColorRect/grid3/Button3.disabled = true
-		$ColorRect/grid4/Button4.disabled = true
+    if selected == true:
+        $ColorRect/grid2/Button2.disabled = true
+        $ColorRect/grid3/Button3.disabled = true
+        $ColorRect/grid4/Button4.disabled = true
 
 func _on_button_hp_pressed() -> void:
-	if selected == false:
-		hp = 5
-		$ColorRect/grid2/hp_text.text = "[right]+ " + str(obj_backup["hp"] + hp) + "[/right]"
-		selected = true
+    if selected == false:
+        hp = 5
+        $ColorRect/grid2/hp_text.text = "[right]+ " + str(obj_backup["hp"] + hp) + "[/right]"
+        selected = true
 
 func _on_button_sp_pressed() -> void:
-	if selected == false:
-		sp = 5
-		$ColorRect/grid3/sp_text.text = "[right]+ " + str(obj_backup["sp"] + sp) + "[/right]"
-		selected = true
+    if selected == false:
+        sp = 5
+        $ColorRect/grid3/sp_text.text = "[right]+ " + str(obj_backup["sp"] + sp) + "[/right]"
+        selected = true
 
 func _on_button_st_pressed() -> void:
-	if selected == false:
-		st = 5
-		$ColorRect/grid4/st_text.text = "[right]+ " + str(obj_backup["st"] + st) + "[/right]"
-		selected = true
+    if selected == false:
+        st = 5
+        $ColorRect/grid4/st_text.text = "[right]+ " + str(obj_backup["st"] + st) + "[/right]"
+        selected = true
 
 func _on_button_con_pressed() -> void:
-	if selected == true:
-		emit_signal("upgrade_and_del", hp, sp, st)
-	
+    if selected == true:
+        emit_signal("upgrade_and_del", hp, sp, st)
+    
 func _on_current_upgrade(obj):
-	obj_backup = obj
-	$ColorRect/grid2/hp_text.text = "[right]+ " + str(obj["hp"]) + "[/right]"
-	$ColorRect/grid3/sp_text.text = "[right]+ " + str(obj["sp"]) + "[/right]"
-	$ColorRect/grid4/st_text.text = "[right]+ " + str(obj["st"]) + "[/right]"
+    obj_backup = obj
+    $ColorRect/grid2/hp_text.text = "[right]+ " + str(obj["hp"]) + "[/right]"
+    $ColorRect/grid3/sp_text.text = "[right]+ " + str(obj["sp"]) + "[/right]"
+    $ColorRect/grid4/st_text.text = "[right]+ " + str(obj["st"]) + "[/right]"
--- script/p_sword.gd
+++ script/p_sword.gd
@@ -7,48 +7,48 @@ var s = sword_def.new().sword_def
 var bonus_damage
 
 func disable_all_sword() -> void:
-	$basic_sword_sprite.hide()
-	$basic_sword_coll.disabled = true
-	$flamming_sword_sprite.hide()
-	$flamming_sword_coll.disabled = true
+    $basic_sword_sprite.hide()
+    $basic_sword_coll.disabled = true
+    $flamming_sword_sprite.hide()
+    $flamming_sword_coll.disabled = true
 
 func enable_sword(sword_sprite, sword_coll) -> void:
-	sword_sprite.show()
-	sword_coll.disabled = false
-	bonus_damage = s[sword_id]["bonus_damage"]
+    sword_sprite.show()
+    sword_coll.disabled = false
+    bonus_damage = s[sword_id]["bonus_damage"]
 
 func current_sword(sword_t: String) -> Array:
-	var ret = [
-		get_node(sword_t + "_sprite"),
-		get_node(sword_t + "_coll")
-	]
-	return ret
+    var ret = [
+        get_node(sword_t + "_sprite"),
+        get_node(sword_t + "_coll")
+    ]
+    return ret
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	disable_all_sword()
-	var sword_type = s[sword_id]["name"]
-	var curs = current_sword(sword_type)
-	enable_sword(curs[0], curs[1])
-	emit_signal("sword_w", s[sword_id]["weight"])
+    disable_all_sword()
+    var sword_type = s[sword_id]["name"]
+    var curs = current_sword(sword_type)
+    enable_sword(curs[0], curs[1])
+    emit_signal("sword_w", s[sword_id]["weight"])
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+    pass
 
 
 func _on_player_hide_sword() -> void:
-	disable_all_sword()
+    disable_all_sword()
 
 
 func _on_player_show_sword() -> void:
-	var sword_type = s[sword_id]["name"]
-	var curs = current_sword(sword_type)
-	enable_sword(curs[0], curs[1])
+    var sword_type = s[sword_id]["name"]
+    var curs = current_sword(sword_type)
+    enable_sword(curs[0], curs[1])
 
 
 func _on_player_rotate_sword(flip) -> void:
-	var sword_type = s[sword_id]["name"]
-	var curs = current_sword(sword_type)
-	curs[0].flip_h = not flip
+    var sword_type = s[sword_id]["name"]
+    var curs = current_sword(sword_type)
+    curs[0].flip_h = not flip
