c06d2a169520b5c861969a53ebaeb931c18f4d1d
Fernando Perry <fernandoperry1234@gmail.com>
2025-01-15 10:41:21 +0700

    small change and effect

--- script/menu.gd
+++ script/menu.gd
@@ -3,6 +3,9 @@ extends Node2D
 @onready var transition = $Transition
 var is_changing_scene = false
 var is_exiting = false
+@onready var t = $blink_timer
+var dim = false
+var strong_duration = 2.0 
 
 func _ready() -> void:
 	$Transition/bg_transition.show()
@@ -10,7 +13,17 @@ func _ready() -> void:
 	await transition.animation_finished
 	$Transition/bg_transition.hide()
 	$Backsound.play()
-	
+	t.start()
+	dim = true
+
+func _process(ft: float) -> void:
+	if dim:
+		$PointLight2D.energy -= 0.1
+		if $PointLight2D.energy < 1.0:
+			$PointLight2D.energy = 1.0  # Prevent energy from going negative
+	else:
+		$PointLight2D.energy = 50
+
 # Button Play func
 func _on_button_play_pressed() -> void:
 	if not is_changing_scene and not is_exiting:
@@ -44,3 +57,14 @@ func _on_transition_animation_finished(anim_name: StringName) -> void:
 			get_tree().change_scene_to_file("res://scene/main.tscn")
 		elif is_exiting:
 			get_tree().quit()
+
+func _on_blink_timer_timeout() -> void:
+	if dim:
+		# Light is currently dimming, set it to strong and start the strong duration timer
+		dim = false
+		t.set_wait_time(strong_duration)
+	else:
+		# Light is currently strong, set it to dim and start the random cooldown timer
+		dim = true
+		t.set_wait_time(randf_range(2.0, 5.0))
+	t.start()
